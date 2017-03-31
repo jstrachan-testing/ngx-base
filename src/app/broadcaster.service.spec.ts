4d1ec85f037c68994dee2811c9f068152e930aef
Dana Gutride <dgutride@redhat.com>
2017-03-31 15:18:07 -0400

    fix(tests): adds broadcaster test

--- src/app/broadcaster.service.spec.ts
+++ src/app/broadcaster.service.spec.ts
@@ -1,5 +1,32 @@
-describe('Broadcaster service', () => {
-  it ('should test the Broadcaster service', function () {
-    expect(1).toBe(1);
+import { inject, TestBed } from '@angular/core/testing';
+import { Broadcaster } from './broadcaster.service';
+
+describe('Service: Broadcaster service', () => {
+
+  let broadcaster: Broadcaster;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        Broadcaster
+      ]
+    });
+  });
+
+  beforeEach(inject(
+    [Broadcaster],
+    (broadcast: Broadcaster) => {
+      broadcaster = broadcast;
+    }
+  ));
+
+  it('Broadcaster can successfully send and receive messages', (done) => {
+    broadcaster.on('testEvent').subscribe((data: number) => {
+      expect(data).toEqual(1001);
+      done();
+    });
+
+    broadcaster.broadcast('testEvent', 1001);
   });
 });
+
